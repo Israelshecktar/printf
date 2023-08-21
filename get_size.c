0dad2d5ad2916dbd52f5af6b16a8437833b3fca5
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-08-21 23:40:05 +0100

    used descriptive variable name

--- get_size.c
+++ get_size.c
@@ -8,18 +8,18 @@
  */
 int get_size(const char *format, int *i)
 {
-	int curr_i = *i + 1;
+	int curr_int = *i + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[curr_int] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	else if (format[curr_int] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = curr_i - 1;
+		*i = curr_int - 1;
 	else
-		*i = curr_i;
+		*i = curr_int;
 
 	return (size);
 }
